6982a7ca061e06403377e0981c91eb42e02608d2
Noor <noormahammad@gmail.com>
2017-07-17 12:02:52 -0400

    Azure AD Authentication using OAuth and OpenID Connect

---
+++ BrowserToWebApp/App_Start/Startup.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Owin;
+using Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(BrowserToWebApp.App_Start.Startup))]
+namespace BrowserToWebApp.App_Start
+{
+    public class Startup
+    {
+        private static string clientId = ConfigurationManager.AppSettings["ida:ClientId"];
+        private static string aadInstance = ConfigurationManager.AppSettings["ida:AADInstance"];
+        private static string tenant = ConfigurationManager.AppSettings["ida:Tenant"];
+        private static string postLogoutRedirectUri = ConfigurationManager.AppSettings["ida:PostLTenantogoutRedirectUri"];
+
+        string authority = string.Format(CultureInfo.InvariantCulture,aadInstance,tenant);
+
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+
+        private void ConfigureAuth(IAppBuilder app)
+        {
+            app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
+            app.UseCookieAuthentication(new CookieAuthenticationOptions());
+
+            app.UseOpenIdConnectAuthentication(
+                new OpenIdConnectAuthenticationOptions
+                {
+                    ClientId = clientId,
+                    Authority = authority,
+                    PostLogoutRedirectUri = postLogoutRedirectUri,
+                    Notifications = new OpenIdConnectAuthenticationNotifications
+                    {
+                        AuthenticationFailed = context =>
+                        {
+                            context.HandleResponse();
+                            context.Response.Redirect("/Error/message=" + context.Exception.Message);
+                            return Task.FromResult(0);
+                        }
+                    }
+                }
+                );
+        }
+
+
+    }
+}
---
+++ BrowserToWebApp/Controllers/AccountController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+
+namespace BrowserToWebApp.Controllers
+{
+
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public void SignIn()
+        {
+            if(!Request.IsAuthenticated)
+            {
+                HttpContext.GetOwinContext().Authentication.Challenge(
+                     new AuthenticationProperties { RedirectUri = "/" }, OpenIdConnectAuthenticationDefaults.AuthenticationType
+                    );
+            }
+        }
+
+        public void SignOut()
+        {
+            if(Request.IsAuthenticated)
+            {
+                HttpContext.GetOwinContext().Authentication.SignOut(
+                    OpenIdConnectAuthenticationDefaults.AuthenticationType, CookieAuthenticationDefaults.AuthenticationType
+                    );
+            }
+        }
+
+    }
+}
